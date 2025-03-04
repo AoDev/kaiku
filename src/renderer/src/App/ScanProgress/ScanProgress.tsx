be9040ac50a976cca96d10b2ec97b20e34452263
AoDev <>
2025-03-04 18:04:53 +0100

    feat(ScanProgress): update styles

--- src/renderer/src/App/ScanProgress/ScanProgress.tsx
+++ src/renderer/src/App/ScanProgress/ScanProgress.tsx
@@ -13,30 +13,38 @@ export const ScanProgress = observer(({rootStore}: {rootStore: RootStore}) => {
   const showMetadataProgress = scanProgress.status === 'complete' && metadataProgress.total > 0
 
   return (
-    <div className="scan-progress">
-      {scanProgress.status === 'starting' && (
-        <div className="scan-progress__message">Starting scan...</div>
-      )}
+    <div className="scan-progress panel--simple pad-default txt-right">
+      {scanProgress.status === 'starting' && <div className="">Starting scan...</div>}
 
       {scanProgress.status === 'scanning' && (
         <div className="scan-progress__message">
-          <div className="scan-progress__spinner" />
-          <span>Scanning: {scanProgress.count} audio files found</span>
+          <div className="flex-row-center gap-1">
+            <h3 className="h3 margin-0">Scanning</h3>
+            <div className="scan-progress__spinner" />
+          </div>
+          <span className="txt-good">{scanProgress.count}</span> audio files found
         </div>
       )}
 
       {showMetadataProgress && metadataProgress.status === 'processing' && (
-        <div className="scan-progress__message">
-          <div className="scan-progress__spinner" />
-          <span>
-            Processing metadata: {metadataProgress.completed} of {metadataProgress.total} files
-          </span>
+        <div className="">
+          <div className="flex-row-center gap-1">
+            <h3 className="h3 margin-0">Processing metadata</h3>
+            <div className="scan-progress__spinner" />
+          </div>
+          <div>
+            <span className="txt-good">{metadataProgress.completed}</span> of{' '}
+            <b>{metadataProgress.total}</b> files
+          </div>
         </div>
       )}
 
       {scanProgress.status === 'complete' && metadataProgress.status === 'complete' && (
-        <div className="scan-progress__message scan-progress__message--complete">
-          Scan complete: {scanProgress.count} audio files processed
+        <div className="">
+          <h3 className="h3 margin-0">Scan complete</h3>
+          <div>
+            <span className="txt-good">{scanProgress.count}</span> audio files processed
+          </div>
         </div>
       )}
     </div>
