076a9cc97cd825f9b7c6598d9eb75c1fdb566964
AoDev <>
2025-03-04 19:23:33 +0100

    chore(ui): add InputX

---
+++ src/renderer/src/ui-framework/components/InputX/InputX.tsx
@@ -0,0 +1,26 @@
+import type {NonFunctionKeys} from 'lib/mobx/store.helpers'
+import {observer} from 'mobx-react'
+import {Input} from '../Input'
+import type {IInputBasicProp} from '../InputBasic'
+
+interface IProps<T> extends IInputBasicProp {
+  id?: string
+  name: Extract<keyof T, string>
+  vm: T & {
+    set: <K extends NonFunctionKeys<T>>(prop: K, value: T[K]) => void
+  }
+}
+
+export function InputX<vm>({vm, name, id, ...otherProps}: IProps<vm>) {
+  return (
+    <Input
+      id={id || (name as string) || undefined}
+      name={name}
+      value={vm[name] as string | number | boolean | undefined}
+      onChangeNameValue={vm.set}
+      {...otherProps}
+    />
+  )
+}
+
+export default observer(InputX)
---
+++ src/renderer/src/ui-framework/components/InputX/index.ts
@@ -0,0 +1,1 @@
+export {default as InputX} from './InputX'
