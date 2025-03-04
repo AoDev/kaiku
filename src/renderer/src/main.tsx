19cd6625f688747a3d26504ddf0c03486b451d16
AoDev <>
2025-03-04 19:24:18 +0100

    chore: remove react strict mode

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,7 +1,6 @@
 import 'virtual:svg-icons-register'
 import './css/index.less'
 
-import React from 'react'
 import {Provider} from 'mobx-react'
 import {createRoot} from 'react-dom/client'
 import {App} from './App'
@@ -9,11 +8,9 @@ import {RootStore} from './stores/RootStore'
 
 const rootStore = new RootStore()
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider rootStore={rootStore}>
-      <App rootStore={rootStore} />
-    </Provider>
-  </React.StrictMode>
+  <Provider rootStore={rootStore}>
+    <App rootStore={rootStore} />
+  </Provider>
 )
 
 if (process.env.NODE_ENV === 'development') {
