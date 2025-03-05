39848291ccc49ca5522c19a2ffea189af26b5368
AoDev <>
2025-03-05 18:54:02 +0100

    feat: display playing song info above playlist

--- src/renderer/src/App/Playlist.tsx
+++ src/renderer/src/App/Playlist.tsx
@@ -1,8 +1,9 @@
 import {observer} from 'mobx-react'
 import type {RootStore} from '@renderer/stores/RootStore'
+import {DEFAULT_ALBUM_COVER} from '@renderer/config'
 
 export const Playlist = observer(({rootStore}: {rootStore: RootStore}) => {
-  const {musicPlayer} = rootStore
+  const {musicPlayer, musicLibrary} = rootStore
 
   const handlePlaySongFromPlaylist = (event: React.MouseEvent<HTMLDivElement>) => {
     const target = event.target as HTMLElement
@@ -13,14 +14,32 @@ export const Playlist = observer(({rootStore}: {rootStore: RootStore}) => {
 
     if (target.tagName === 'DIV') {
       const songIndex = Number(target.dataset.songIndex)
-      if (!isNaN(songIndex)) {
+      if (isFinite(songIndex)) {
         musicPlayer.play(songIndex)
       }
     }
   }
 
+  const album = musicLibrary.albums.find((album) => album.id === musicPlayer.song?.albumId)
+  const coverPath = album?.coverExtension
+    ? `cover://${album.id}.${album.coverExtension}`
+    : DEFAULT_ALBUM_COVER
+
   return (
-    <div className="playlist library__col" onClick={handlePlaySongFromPlaylist}>
+    <div className="playlist" onClick={handlePlaySongFromPlaylist}>
+      <div className="margin-bottom-1">
+        {musicPlayer.song && (
+          <>
+            {/* biome-ignore lint/a11y/useAltText: We do not have text description of each cover */}
+            <img className="playlist__cover" src={coverPath} />
+            <b>{musicPlayer.song.title}</b>
+            <div>
+              <span className="txt-unit">by</span> {musicPlayer.song.artist}{' '}
+              <span className="txt-unit">on</span> {musicPlayer.song.album}
+            </div>
+          </>
+        )}
+      </div>
       {musicPlayer.playlist.map((song, index) => (
         <div
           className={`pad-left-0 ${musicPlayer.playlistIndex === index ? 'row--playing' : 'row'}`}
