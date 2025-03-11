7495a3867e45d3ab7f2bb30c6901b4a95dc4581f
AoDev <>
2025-03-11 11:20:46 +0100

    fix(Footer): background

--- src/renderer/src/App/Footer/Footer.tsx
+++ src/renderer/src/App/Footer/Footer.tsx
@@ -3,17 +3,18 @@ import type {FooterVM} from './FooterVM'
 import {ScanProgress} from './ScanProgress'
 
 export const Footer = observer(({vm}: {vm: FooterVM}) => {
-  const {musicLibrary} = vm.rootStore
-  const {scanProgress} = musicLibrary
+  const {scanProgress} = vm.rootStore.musicLibrary
 
   if (scanProgress.status === 'idle') {
     return null
   }
 
   return (
-    <div className="footer flex-row-center gap-1 justify-between zoom-in">
-      <div className="flex-row-center gap-1" />
-      <ScanProgress rootStore={vm.rootStore} />
+    <div className="footer zoom-in">
+      <div className="bg-alternative flex-row-center gap-1 justify-between">
+        <div className="flex-row-center gap-1" />
+        <ScanProgress rootStore={vm.rootStore} />
+      </div>
     </div>
   )
 })
