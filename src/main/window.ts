92e2f1a7ede22ff89ab44f1eca81c558dd6c8b71
AoDev <>
2025-03-13 23:03:23 +0100

    chore: start app with a bigger window (fullscreen under a certain limit)

--- src/main/window.ts
+++ src/main/window.ts
@@ -1,18 +1,30 @@
 import {join} from 'node:path'
 import {is} from '@electron-toolkit/utils'
-import {BrowserWindow, shell} from 'electron'
+import {BrowserWindow, screen, shell} from 'electron'
 
 export function createWindow(): BrowserWindow {
-  const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+  // Get the primary display's size
+  const primaryDisplay = screen.getPrimaryDisplay()
+  const {width: screenWidth, height: screenHeight} = primaryDisplay.workAreaSize
+
+  // Determine window settings based on screen width and height
+  const windowOptions = {
+    width: screenWidth > 1600 ? 1600 : screenWidth,
+    height: screenHeight > 982 ? 982 : screenHeight,
     show: false,
     autoHideMenuBar: true,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false,
     },
-  })
+  }
+
+  const mainWindow = new BrowserWindow(windowOptions)
+
+  // If screen dimensions are larger than our limits, center the window
+  if (screenWidth > 1600 || screenHeight > 900) {
+    mainWindow.center()
+  }
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
