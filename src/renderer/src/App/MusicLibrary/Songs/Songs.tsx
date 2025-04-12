22e705a39e4cf52ba110f3f3940db976e2a00612
AoDev <>
2025-04-12 13:02:10 +0200

    feat(Songs): show individual songs artists if the album has at least 3 artists (compilations)

--- src/renderer/src/App/MusicLibrary/Songs/Songs.tsx
+++ src/renderer/src/App/MusicLibrary/Songs/Songs.tsx
@@ -1,8 +1,13 @@
 import {getAlbumCover} from '@rootsrc/renderer/src/config'
+import type {Song} from '@rootsrc/types/MusicLibrary.types'
 import {observer} from 'mobx-react'
 import type {MusicLibraryVM} from '../MusicLibraryVM'
 import {ContextMenu} from '../shared/ContextMenu'
 import {SongDetails} from './SongDetails'
+
+function getArtists(songs: Song[]) {
+  return songs.reduce((acc: Set<string>, song) => acc.add(song.artist), new Set<string>())
+}
 
 export const Songs = observer(({vm}: {vm: MusicLibraryVM}) => {
   const {musicLibrary, musicPlayer} = vm.rootStore
@@ -10,6 +15,8 @@ export const Songs = observer(({vm}: {vm: MusicLibraryVM}) => {
   const needsDetails =
     (filter !== null && musicLibrary.albumSelected === '') ||
     musicLibrary.filteredAlbums.length === 0
+  // In case of an album with more than 2 artists, let's show the artists (compilations, soundtracks)
+  const needsArtists = musicLibrary.albumSelected && getArtists(musicLibrary.filteredSongs).size > 2
 
   return (
     <>
@@ -34,12 +41,17 @@ export const Songs = observer(({vm}: {vm: MusicLibraryVM}) => {
                 </div>
                 {songs.map((song) => (
                   <div
-                    className={`library-item ${musicLibrary.songSelected === song.filePath ? 'selected' : ''} ${musicPlayer.song === song ? 'playing' : ''}`}
+                    className={`library-item flex-row-center justify-between gap-1 ${musicLibrary.songSelected === song.filePath ? 'selected' : ''} ${musicPlayer.song === song ? 'playing' : ''}`}
                     key={song.filePath}
                     data-file-path={song.filePath}
                   >
-                    {song.disk.of > 1 && <span className="txt-unit">CD{song.disk.no} - </span>}
-                    <span className="txt-muted">{song.trackNumber}.</span> {song.title}
+                    <div>
+                      {song.disk.of > 1 && <span className="txt-unit">CD{song.disk.no} - </span>}
+                      <span className="txt-muted">{song.trackNumber}.</span> {song.title}
+                    </div>
+                    {needsArtists && (
+                      <span className="txt-muted nowrap-truncate">{song.artist}</span>
+                    )}
                   </div>
                 ))}
               </div>
